eb9910e60b587cc3941f0733e4b661f966b48f3d
Saumiya <itssaumiya@gmail.com>
2024-01-25 17:34:46 +0400

    added 1

--- Script1.sh
+++ Script1.sh
@@ -5,5 +5,5 @@ echo "Hello, How are you?"
 echo "Welcome to Young Minds"
 echo "Hello batch-21, We are learning Jenkins"
 echo "Good morning batch-22, We are learning GitHub"
-echo "We are learning branching strategies"
+echo "We are learning branching strategies, changes made"
 
